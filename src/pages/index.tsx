4a05759b6fcefb8a8f14aaa29dc1152596d5b780
Vaibhav Kubre <v@kubre.in>
2023-04-17 09:59:14 +0530

    Able to get photos from pexels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "app/utils/api";
+import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 import srtParser2 from "srt-parser-2";
+
 const srtParser = new srtParser2();
 
-const makePrompt = (line: string) => {
-  return `I want you to act as a text terminal that only answers in a single line with no labels. If asked about dogs your answer should be "Dog, Animal, Nature, Friend" and nothing else
-Question. \nFollowing is the text from part of the audio clip can you in 1-2 words describe the subject of the text also 3-4 related concepts. Make sure to not add anything else \n """${line}"""`;
+const makePromptForPexels = (lines: string[]) => {
+  return `You're a program, which takes N lines separated by new lines as input. All the lines are related. Your task is to for each line detect subject of that line and output it in 1-2 words. Output must be in N lines. \n Input: """${lines.join(
+    " \n "
+  )}""" \n Output: `;
 };
+
+const RECORDING_LIMIT_MS = 31_000;
 
 const Home: NextPage = () => {
   const audioElement = useRef<HTMLAudioElement | null>(null);
   const [id, setId] = useState<NodeJS.Timeout>();
-  const [openAiToken, setOpenAiToken] = useState<string>();
-  const [pexelsToken, setPexelsToken] = useState<string>();
+  const [openAiToken, setOpenAiToken] = useState<string>("");
+  const [pexelsToken, setPexelsToken] = useState<string>("");
   const [srtString, setSrtString] = useState<string>();
+  const [imageElems, setImageElems] = useState<string>("");
 
   const startRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -37,7 +40,6 @@ const Home: NextPage = () => {
       const blob = new Blob(chunks);
       console.log(blob.size);
       submitAudio(blob).catch(console.error);
-      // setAudio(blob);
       const audioURL = URL.createObjectURL(blob);
       if (audioElement.current) {
         audioElement.current.src = audioURL;
@@ -53,7 +55,7 @@ const Home: NextPage = () => {
         console.log("stopped");
         clearTimeout(id);
         setId(undefined);
-      }, 5000)
+      }, RECORDING_LIMIT_MS)
     );
   };
 
@@ -76,61 +78,67 @@ const Home: NextPage = () => {
     setSrtString(data);
     const concepts = (await extractSubjects(data).catch(console.error)) ?? [];
     const photos = (await getImages(concepts).catch(console.error)) ?? [];
+    const makeImg = (url: string) =>
+      `<img src="${url}" style="height: 200px; width: auto" />`;
+    setImageElems(photos.map(makeImg).join(""));
   };
 
   const extractSubjects = async (srtString: string | undefined) => {
     if (!srtString || !openAiToken) return [];
     const srtData = srtParser.fromSrt(srtString);
-    const prompts = srtData.map((line) => makePrompt(line.text));
-    const concepts: string[] = [];
-    concepts.push(
-      await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${openAiToken}`,
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          max_tokens: 300,
-          temperature: 0.0,
-          messages: [{ role: "user", content: prompts[0] }],
-        }),
-      })
-        .then(
-          (res) =>
-            res.json() as Promise<{
-              choices: { message: { content: string } }[];
-            }>
-        )
-        .then((res) => res.choices?.[0]?.message.content || "")
-    );
-    console.log("Concepts found: ", concepts);
-    return concepts.filter((concept) => concept.length);
+    const userPrompt = makePromptForPexels(srtData.map((item) => item.text));
+    const concepts = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${openAiToken}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        max_tokens: 300,
+        temperature: 0.0,
+        messages: [{ role: "user", content: userPrompt }],
+      }),
+    })
+      .then(
+        (res) =>
+          res.json() as Promise<{
+            choices: { message: { content: string } }[];
+          }>
+      )
+      .then((res) => res.choices?.[0]?.message.content || "")
+      .then((res) =>
+        res
+          .split("\n")
+          .map((line) => line.trim())
+          .filter((line) => line.length)
+      );
+    return concepts;
   };
 
   const getImages = async (concepts: string[]) => {
     if (!concepts[0] || !pexelsToken) return [];
 
-    const photos = [];
-    photos.push(
-      await fetch(
-        `https://api.pexels.com/v1/search?query=${concepts[0]}&per_page=1`,
-        {
-          headers: {
-            Authorization: pexelsToken,
-          },
-        }
+    const photos = await Promise.all(
+      concepts.map(
+        async (concept) =>
+          await fetch(
+            `https://api.pexels.com/v1/search?query=${concept}&per_page=1`,
+            {
+              headers: {
+                Authorization: pexelsToken,
+              },
+            }
+          )
+            .then(
+              (res) =>
+                res.json() as Promise<{
+                  photos: { src: { portrait: string } }[];
+                }>
+            )
+            .then((res) => res.photos?.[0]?.src.portrait || "")
       )
-        .then(
-          (res) =>
-            res.json() as Promise<{
-              photos: { src: { portrait: string } }[];
-            }>
-        )
-        .then((res) => res.photos?.[0]?.src.portrait || "")
     );
-    console.log("Photos found: ", photos);
     return photos
       .filter((photo) => photo.length)
       .map((photo) => {
@@ -207,6 +215,10 @@ const Home: NextPage = () => {
             <pre className="border-1 max-w-screen-md overflow-x-scroll rounded-lg border-zinc-500 bg-zinc-900 p-4 text-white">
               <code>{srtString}</code>
             </pre>
+            <div
+              className="mt-4 flex gap-x-2"
+              dangerouslySetInnerHTML={{ __html: imageElems }}
+            ></div>
             {/* <AuthShowcase /> */}
           </div>
         </div>
@@ -220,16 +232,10 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
